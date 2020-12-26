b620e12cd687658d90c875e6193574e4cb6a7788
KazumaShachou <raianimes1000@gmail.com>
2020-12-26 14:54:14 -0400

    localization button and animations

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -9,7 +9,7 @@ const icon = L.icon({
     iconUrl : "./public/images/map-marker.svg",
     iconSize : [58, 68],
     iconAnchor : [29, 68],
-    popupAnchor : [170,2]
+    popupAnchor : [170, 2]
 })
 
 //create popup overlay
@@ -18,9 +18,10 @@ const popup = L.popup({
     className : 'map-popup',
     minWidht : 240,
     minHeight: 240
-}).setContent('Peko-chan no IE <a href="orphanages.html?id=1" class= "choose-orphanage"> img src = "./public/images/arrow-white.svg" </a>')
+}).setContent('Peko-chan no IE <a href="orphanages.html?id=1" class= "choose-orphanage"> <img src = "./public/images/arrow-white.svg"> </a>')
 
 //create add and marker
-L.marker([35.6669392,139.6574953]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup()
+L.marker([35.6669392,139.6574953], {icon})
+.addTo(map)
+.bindPopup(popup)
+ 
